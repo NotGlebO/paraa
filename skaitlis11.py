fb0a2d5d63792d9f7b77e407f4765a3518b97af7
NotGlebO <77108978+NotGlebO@users.noreply.github.com>
2022-04-28 15:40:54 +0300

    Create skaitlis11.py

---
+++ skaitlis11.py
@@ -0,0 +1,11 @@
+a = int(input("Ievadī pirmo skaitļu "))
+b = int(input("Ievadī otro skaitļu "))
+
+if a > b:
+    print("Pirmo skaitļa vairak neka otro")
+    
+elif a = b:
+    print("Viņi vienadi")
+    
+elif a < b:
+    print("Otro vairak")
